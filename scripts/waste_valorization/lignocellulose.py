a8d17590aaeca103d8d9b46202e1b76b01cc9ddc
Yalin <mailto.yalin.li@gmail.com>
2024-05-27 13:36:36 +0800

    update to fix flow adjusting issue

--- scripts/waste_valorization/lignocellulose.py
+++ scripts/waste_valorization/lignocellulose.py
@@ -13,11 +13,24 @@ Please refer to https://github.com/QSD-Group/QSDsan/blob/main/LICENSE.txt
 for license details.
 '''
 
+import biosteam as bst
 from biorefineries.cellulosic import Biorefinery as CellulosicEthanol
 from biorefineries.cellulosic.streams import cornstover as cornstover_kwargs
 from biorefineries.cornstover import ethanol_density_kggal
 
-# Common settings for both biorefineries
+
+
+br = CellulosicEthanol(
+    name='ethanol',
+    )
+
+sys = br.sys
+tea = sys.TEA
+f = sys.flowsheet
+stream = f.stream
+feedstock = stream.cornstover
+ethanol = stream.ethanol
+
 #!!! Need to update the composition, etc.
 # here's what `cornstover_kwargs` looks like
 # cornstover = stream_kwargs(
@@ -38,47 +51,33 @@ from biorefineries.cornstover import ethanol_density_kggal
 #     units='kg/hr',
 #     price=0.05158816935126135,
 # )
-feedstock_kwargs = cornstover_kwargs.copy()
-feedstock_kwargs.update({
-    'ID': 'lignocellulose',
-    'Glucan': 0.28,
-    'total_flow': 1000,
-    })
 
+feedstock.F_mass = 1000 # kg/hr
 
 #!!! Needs updating, put the baseline values here, current numbers are placeholders
 prices = {
-    'lignocellulose': 0.2, # $/kg including water
-    'Electricity': 0.07,
+    'cornstover': 0.2, # $/kg including water
     }
+for ID, price in prices.items(): stream.search(ID).price = price
+bst.PowerUtility.price = 0.07
 
 GWP_CFs = {
-    'lignocellulose': 1.,
+    'cornstover': 0.2,
     'sulfuric_acid': 1,
     'ammonia': 1,
     'cellulase': 1, #!!! note water content
     'CSL': 1,
     'caustic': 1, #!!! note water content    
     'FGD_lime': 1, #!!! need to be clear if this is CaO or Ca(OH)2
-    'Electricity': (1., 1.,), # consumption, production
     }
+for ID, CF in GWP_CFs.items(): stream.search(ID).characterization_factors['GWP'] = CF
+bst.PowerUtility.characterization_factors['GWP'] = (1., 1.,) # consumption, production
 
-br = CellulosicEthanol(
-    name='ethanol',
-    feedstock_kwargs=feedstock_kwargs,
-    prices=prices,
-    GWP_CFs=GWP_CFs,
-    )
-
-sys = br.sys
 sys.simulate()
-tea = sys.TEA
-
-f = sys.flowsheet
-ethanol = f.stream.ethanol
-
+get_ethanol = lambda: ethanol.F_mass*ethanol_density_kggal*tea.operating_hours/1e6 # MM gal/year
 get_MESP = lambda: tea.solve_price(ethanol)*ethanol_density_kggal # from $/kg to $/gallon
 get_GWP = lambda: sys.get_net_impact('GWP')/sys.operating_hours/ethanol.F_mass*ethanol_density_kggal
 
+print(f'annual ethanol: ${get_ethanol():.0f} MM gal/yr')
 print(f'price: ${get_MESP():.3f}/gal')
 print(f'GWP: {get_GWP():.3f} kg CO2e/gal')
